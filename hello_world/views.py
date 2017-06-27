515f03d4aedfdb271b5fb18c43c448a980eafe32
Jenu Patel <jenu.16@icloud.com>
2017-06-27 01:46:20 -0500

    Added text-based views and configured urls

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('<html><body>Hello, World!</body></html>')
+
+def about(request):
+    msg = "Here is the About Page. Want to return home? \
+    <a href='/'>Back Home</a>"
+    return HttpResponse(msg)
